99ad197081f8396e236b2605a20e13811d1711e5
bot-fluttercrew <bot.fluttercrew@gmail.com>
2021-02-28 15:21:25 +0200

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## [0.0.1] - TODO: Release.
 
 * Initial release.
+
+## [0.0.2] - TODO: Add Utils.
+
+* Added utils.
--- lib/fluttercrew.dart
+++ lib/fluttercrew.dart
@@ -1,3 +1,4 @@
 library fluttercrew;
 
 export 'src/fluttercrew_singletons.dart';
+export 'src/fluttercrew_utils.dart';
